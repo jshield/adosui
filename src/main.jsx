6c629c82cb646d4dd941211f8379dec27719ee18
Daedulus <52508+jshield@users.noreply.github.com>
2026-04-13 15:39:53 +0000

    feat: Add eruda dev console (access via #eruda hash)

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './ui.jsx'
 
+if (window.location.hash === '#eruda') {
+  import('eruda').then(module => {
+    const eruda = module.default
+    eruda.init()
+  })
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
